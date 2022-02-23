9a3b1efa9856b57c029b6bef0e6b41ba61ca9c31
Angella <sofibaigorria@gmail.com>
2022-02-23 01:23:02 -0500

    error propogation

--- error_prop.py
+++ error_prop.py
@@ -17,10 +17,9 @@ F = (D*(2*h*he)**(1/2))/L
 
 files = ["mb1.csv","mb2.csv","pb1.csv","pb2.csv"]
 count = 0
-
+error_list = []
 for file in files:
     file_data = pd.read_csv(file)
-    print("\n"+ file)
     h1 = file_data["h1 (cm)"][0]
     h2 = file_data["h2 (cm)"][0]
     h1p = file_data["h1' (cm)"][0]
@@ -31,30 +30,35 @@ for file in files:
     delta_p = h1p - h2p
 
     he_val = (10/7)*(delta_h - delta_p)
+    sigma_he = (10/7)*(4*0.005**2)**(1/2)
+    sigma = 0.005
+    
+    d_val = file_data["D (cm)"][0]
+    l_val = file_data["L (cm)"][0]
+
 
 # Here we are calculating for the partial derivative of the equation
-sigma = 0.05
-d_val = [.298, .298, .276, .292]
-h2_val = [1.144,1.123,1.076, 1.046]
-he_val = []
-L_val = [0.294,0.294,0.294,0.294]
+    F_D = F.diff(D)
+    partial_D = F_D.evalf(subs= {D:d_val, h: h2, he: he_val, L: l_val})
 
-F_D = F.diff(D)
-print(F_D)
+    F_h = F.diff(h)
+    partial_h = F_h.evalf(subs= {D:d_val, h: h2, he: he_val, L: l_val})
 
+    F_he = F.diff(he)
+    partial_he = F_he.evalf(subs= {D:d_val, h: h2, he: he_val, L: l_val})
 
-F_h = F.diff(h)
-print(F_h)
+    F_L = F.diff(L)
+    partial_L = F_L.evalf(subs= {D:d_val, h: h2, he: he_val, L: l_val})
 
-
-F_he = F.diff(he)
-print(F_he)
-
-
-F_L = F.diff(L)
-print(F_L)
-
-    #partial = Fx(final_vel,initial_vel)
+    error = math.sqrt((partial_D)*(sigma**2)+(partial_h)*(sigma**2)+(partial_he)*(sigma**2)+(partial_L)*(sigma**2))
+    error_list.append(error)
+    print("\n"+ file)
+    print("Error: "+ str(error))
+    
+   
+print(error_list)
+    #sigma_ej_list.append(sigma_ej)
+#partial = Fx(final_vel,initial_vel)
 #partial_x = Fx.evalf(subs = {x:list(final_vel)[trial], y:list(initial_vel)[trial]})
 #print(Fx)
 

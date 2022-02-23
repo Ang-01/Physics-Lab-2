d38db5604687351045af89fa28e0fae418ab1900
Angella <sofibaigorria@gmail.com>
2022-02-22 19:58:03 -0500

    new files

---
+++ error_prop.py
@@ -0,0 +1,5 @@
+import numpy as np    # Note you can also import functions as abbreviations
+import matplotlib.pyplot as plt 
+import csv
+import pandas as pd
+
---
+++ prediction.py
@@ -0,0 +1,40 @@
+import math
+def predict(h1_p, h2_p, h1, h2, h3, D, L):
+    delta_h_p = h1_p - h2_p
+    delta_h = h1 - h2
+    v_0 = math.sqrt((10/7)*9.806*(delta_h-delta_h_p))
+    v_0x = v_0*D/L
+    v_0y = v_0*(h2-h3)/L
+    t = v_0y/9.806 + (math.sqrt(v_0y**2+2*9.806*h2))/9.806
+    return t*v_0x
+
+def test(a,b):
+    return a**2 + b
+
+e = predict(1.216	,1.167,	1.37,	1.045	,.996, .292	,	.294)
+
+
+# New Trial 1 Metal Ball with premal values
+f = predict(1.216	,1.167,	1.249,	1.137	,1.054, .292	,	.294)
+# New Trial 2 Metal Ball with premal values
+g = predict(1.205,	1.18,	1.271,	1.123,	1.045,	.298	,.294)
+#^^ we changed D
+i = predict(1.205,	1.18,	1.271,	1.123,	1.045,	.285	,.294)
+# chnaging thr d to see if it matches
+k = predict(1.205,	1.18,	1.271,	1.123,	1.045,	.290	,.294)
+#regular Trial 2 with new h3
+h = predict(1.205,	1.18,	1.283,	1.121,	1.05,	.298	,.294)
+print(e)
+print(f)
+print(g)
+print(i)
+print(k)
+
+# first trial with metal ball
+p = predict(1.205,	1.18,1.26,	1.144,	1.068,	.298,	.294)
+print(p)
+t = predict(1.227,1.205,1.243,1.193,1.110,0.277,0.292)
+print(t)
+
+w = predict(1.205,	1.18,	1.283,	1.121,	1.05,	.298	,.294)
+print(w)

8e6f41ea6651f108611e40c897d1247ce2b30ec1
Angella <sofibaigorria@gmail.com>
2022-02-23 00:35:24 -0500

    new data

--- error_prop.py
+++ error_prop.py
@@ -6,20 +6,54 @@ import math
 from sympy import symbols, diff 
 
 # Equation needed in order to calculate partial derivatives
-D, h2, he, L = symbols('D, h2, he, L', real = True) 
-F = (D)*math.sqrt(2*(h2)*(he))/(L)
+# Dont use sqrt use **(1/2) instead
+D, h, he, L = symbols('D, h, he, L', real = True) 
+#he = (10/7)*(4*0.005**2)**(1/2)
+F = (D*(2*h*he)**(1/2))/L
 
 # This is where we calculate for the coeffcient of Restitution
 # there are 10 values for the 10 trials we did
 
 
+files = ["mb1.csv","mb2.csv","pb1.csv","pb2.csv"]
+count = 0
 
+for file in files:
+    file_data = pd.read_csv(file)
+    print("\n"+ file)
+    h1 = file_data["h1 (cm)"][0]
+    h2 = file_data["h2 (cm)"][0]
+    h1p = file_data["h1' (cm)"][0]
+    h2p = file_data["h2' (cm)"][0]
+
+    #print(int(h1)/100)
+    delta_h = h1 - h2
+    delta_p = h1p - h2p
+
+    he_val = (10/7)*(delta_h - delta_p)
 
 # Here we are calculating for the partial derivative of the equation
+sigma = 0.05
+d_val = [.298, .298, .276, .292]
+h2_val = [1.144,1.123,1.076, 1.046]
+he_val = []
+L_val = [0.294,0.294,0.294,0.294]
 
-Fx = F.diff(D)
-print("hello")
-print(Fx)
+F_D = F.diff(D)
+print(F_D)
+
+
+F_h = F.diff(h)
+print(F_h)
+
+
+F_he = F.diff(he)
+print(F_he)
+
+
+F_L = F.diff(L)
+print(F_L)
+
     #partial = Fx(final_vel,initial_vel)
 #partial_x = Fx.evalf(subs = {x:list(final_vel)[trial], y:list(initial_vel)[trial]})
 #print(Fx)
--- prediction.py
+++ prediction.py
@@ -25,16 +25,33 @@ k = predict(1.205,	1.18,	1.271,	1.123,	1.045,	.290	,.294)
 #regular Trial 2 with new h3
 h = predict(1.205,	1.18,	1.283,	1.121,	1.05,	.298	,.294)
 print(e)
-print(f)
-print(g)
-print(i)
+print(str(f) + "hello")
+print(str(g)+"metal trial 2")
+print(str(i)+"better")
 print(k)
-
+print(h)
 # first trial with metal ball
 p = predict(1.205,	1.18,1.26,	1.144,	1.068,	.298,	.294)
-print(p)
+print(str(p)+ "first trial metal ball")
 t = predict(1.227,1.205,1.243,1.193,1.110,0.277,0.292)
 print(t)
 
 w = predict(1.205,	1.18,	1.283,	1.121,	1.05,	.298	,.294)
 print(w)
+
+
+#Plastic Ball Trial 1
+pb1 = predict(1.216,1.167,	1.32,	1.074,	1.017,	.276,.294)
+print("pb1: " + str(pb1))
+
+#Plastic Ball Trial 2
+pb2 = predict(1.216,1.167,	1.35,1.046,	.996,	.292,	.294)
+print("pb2: " + str(pb2))
+
+#Metal Ball Trial 1
+mb1 = predict(1.205,	1.18,	1.26,	1.144,	1.068,	.298,	.294)
+print("mb1: " + str(mb1))
+
+#Metal Ball Trial 2
+mb2 = predict(1.205,	1.18,	1.271,	1.123,	1.045,	.298	,.294)
+print("mb2: " + str(mb2))
